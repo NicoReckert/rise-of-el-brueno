1b5459e4cae9ec028791eec54cc02f989f340038
Nico Reckert <n.r-86@gmx.de>
2026-02-20 08:38:31 +0100

    In the essence-trail-particle class, all functions are divided so that they have a maximum of 14 lines and js doc is added to each function.

--- classes/effects/essence-trail-particle.class.js
+++ classes/effects/essence-trail-particle.class.js
@@ -1,35 +1,57 @@
 import { MovableObject } from '../systems/movable-object.class.js';
-
+/**
+ * Represents a particle used for an essence trail effect.
+ */
 export class EssenceTrailParticle extends MovableObject {
+  /**
+  * Creates a new instance.
+  * @param {*} img Image source.
+  * @param {number} x Initial x-coordinate.
+  * @param {number} y Initial y-coordinate.
+  */
   constructor(img, x, y) {
     super();
-    this.img = img;         // Image-Objekt (z.B. Essence Frame)
+    this.initBaseProperties(img, x, y);
+    this.initRandomVelocity();
+  }
+
+  /**
+  * Initializes base properties for the particle.
+  * @param {*} img Image source.
+  * @param {number} x Initial x-coordinate.
+  * @param {number} y Initial y-coordinate.
+  */
+  initBaseProperties(img, x, y) {
+    this.img = img;
     this.x = x;
     this.y = y;
     this.width = 18;
     this.height = 18;
-
     this.opacity = 0.6;
-    this.life = 260;        // ms
+    this.life = 260;
     this.birth = null;
-
-    // mini drift
-    this.vx = (Math.random() - 0.5) * 0.6;
-    this.vy = (Math.random() - 0.5) * 0.6;
-
     this.isFinished = false;
   }
 
+  /**
+  * Initializes a random velocity for the particle.
+  */
+  initRandomVelocity() {
+    this.vx = (Math.random() - 0.5) * 0.6;
+    this.vy = (Math.random() - 0.5) * 0.6;
+  }
+
+  /**
+  * Updates the particle state.
+  * @param {number} timestamp Frame timestamp.
+  */
   updateState(timestamp) {
     if (!this.birth) this.birth = timestamp;
     const t = timestamp - this.birth;
-
     this.x += this.vx;
     this.y += this.vy;
-
     const p = Math.min(1, t / this.life);
     this.opacity = 0.6 * (1 - p);
-
     if (p >= 1) this.isFinished = true;
   }
 }
