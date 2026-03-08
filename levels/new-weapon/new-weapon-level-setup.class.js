90504ed3667f0e35282e3c4402185a8db5b18c53
Nico Reckert <n.r-86@gmx.de>
2026-03-08 22:23:37 +0100

    All functions of the new weapon level setup class reduced to a maximum of 14 lines and js doc added.

--- levels/new-weapon/new-weapon-level-setup.class.js
+++ levels/new-weapon/new-weapon-level-setup.class.js
@@ -1,18 +1,65 @@
 import { AnimatedEntity } from '../../classes/entities/animated-entity.class.js';
 
+/**
+ * Sets up the new weapon level environment, sounds, and state.
+ */
 export class NewWeaponLevelSetup {
+    /**
+     * Creates a new level scene controller instance.
+     * @param {Object} world World reference.
+     */
     constructor(world) {
         this.world = world;
+        this.initWorldRefs();
+        this.environment = this.createEnvironment();
+        this.sounds = this.createSounds();
+        this.setupSceneState();
+    }
+
+    /**
+     * Initializes world-related references.
+     * @returns {void}
+     */
+    initWorldRefs() {
         this.entityImages = this.world.entityImages;
         this.allAudios = this.world.allAudios;
         this.allVideos = this.world.allVideos;
-        this.environment = {
-            macuahuitl: new AnimatedEntity(this.entityImages, 'macuahuitl', 300, 300, 700, 300) //412 x 412, 650, 250
+    }
+
+    /**
+     * Creates the environment entities for the level.
+     * @returns {Object} Environment map.
+     */
+    createEnvironment() {
+        return {
+            macuahuitl: this.createMacuahuitl()
         };
-        this.sounds = {
+    }
+
+    /**
+     * Creates the macuahuitl entity.
+     * @returns {Object} Animated entity instance.
+     */
+    createMacuahuitl() {
+        return new AnimatedEntity(this.entityImages, 'macuahuitl', 300, 300, 700, 300);
+    }
+
+    /**
+     * Creates the sound references for the level.
+     * @returns {Object} Sound map.
+     */
+    createSounds() {
+        return {
             newWeaponMusic: this.allAudios.newWeaponMusic,
             newWeaponSpeakSound: this.allAudios.newWeaponSpeakSound
         };
+    }
+
+    /**
+     * Initializes the scene state for the level.
+     * @returns {void}
+     */
+    setupSceneState() {
         this.sounds.newWeaponMusic.volume = 0.8;
         this.environment.macuahuitl.isFlipped = false;
         this.video = this.allVideos.new_weapon_bg_video || null;
