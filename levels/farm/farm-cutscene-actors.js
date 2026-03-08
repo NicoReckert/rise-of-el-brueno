a26d8940a1ccaa21a5d97a1f6e147074eab05a26
Nico Reckert <n.r-86@gmx.de>
2026-03-08 22:01:49 +0100

    Added to farm-cutscene-actors.js js doc

--- levels/farm/farm-cutscene-actors.js
+++ levels/farm/farm-cutscene-actors.js
@@ -1,13 +1,16 @@
 import { AnimatedEntity } from "../../classes/entities/animated-entity.class.js";
 
+/**
+ * Creates the farm cutscene actor instances.
+ * @param {Object} entityImages Entity image sources.
+ * @returns {Object} Actor map.
+ */
 export function createFarmCutsceneActors(entityImages) {
     const actors = {
         chickenTranced: new AnimatedEntity(entityImages, 'chickenTranced', 90, 90, 200, 580),
         chickTranced: new AnimatedEntity(entityImages, 'chickTranced', 60, 60, 500, 600),
         cowTranced: new AnimatedEntity(entityImages, 'cowTranced', 200, 200, -100, 492),
     };
-
     actors.cowTranced.isFlipped = true;
-
     return actors;
 }
