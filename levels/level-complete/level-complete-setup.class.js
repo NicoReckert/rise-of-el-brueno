c5f9fe7d7c17c4c7f41e4d89ec8dffa245c12156
Nico Reckert <n.r-86@gmx.de>
2026-03-09 17:04:12 +0100

    All functions of the level complete setup class reduced to a maximum of 14 lines and Js doc added.

--- levels/level-complete/level-complete-setup.class.js
+++ levels/level-complete/level-complete-setup.class.js
@@ -1,20 +1,43 @@
 import { AnimatedEntity } from '../../classes/entities/animated-entity.class.js';
 import { levelCompleteEvents } from '../../events/level-complete-events.js';
 
+/**
+ * Sets up the level complete scene including characters, sounds, and video.
+ */
 export class LevelCompleteSetup {
+    /**
+     * Creates a new level complete setup instance.
+     * @param {Object} world World reference.
+     */
     constructor(world) {
         this.world = world;
-        this.entityImages = this.world.entityImages;
-        this.allAudios = this.world.allAudios;
-        this.allVideos = this.world.allVideos;
+        this.entityImages = world.entityImages;
+        this.allAudios = world.allAudios;
+        this.allVideos = world.allVideos;
         this.levelCompleteEvents = levelCompleteEvents;
-        this.characters = {
+        this.characters = this.createCharacters();
+        this.sounds = this.createSounds();
+        this.video = this.allVideos.complete_bg_video || null;
+    }
+
+    /**
+     * Creates the level complete character instances.
+     * @returns {Object} Character map.
+     */
+    createCharacters() {
+        return {
             levelCompleteCharacter: new AnimatedEntity(this.entityImages, 'levelCompleteCharacter', 512, 512, 410, 180)
         };
-        this.sounds = {
+    }
+
+    /**
+     * Creates the level complete sound references.
+     * @returns {Object} Sound map.
+     */
+    createSounds() {
+        return {
             levelCompleteMusic: this.allAudios.levelCompleteMusic,
             levelCompleteSound: this.allAudios.levelCompleteSound
         };
-        this.video = this.allVideos.complete_bg_video || null;
     }
 }
